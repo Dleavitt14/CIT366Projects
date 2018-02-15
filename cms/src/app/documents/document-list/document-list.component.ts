3873fb7c7c6eec296fec96cfa4ac083963ae479a
Derek <ddleavitt14@gmail.com>
2018-02-15 12:42:43 -0700

    2/15/18 lesson 4

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Document } from '../document.model';
+import {Contacts} from '../../contacts/contacts.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentListComponent implements OnInit {
 
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  document: Document[] = [
+    new Document (1, 'file1', 'file for school', 'fakeURL1', 'noChildren'),
+    new Document (2, 'file2', 'file for work', 'fakeURL2', 'noChildren'),
+    new Document (3, 'file3', 'file for fun', 'fakeURL3', 'noChildren'),
+    new Document (4, 'file4', 'file for taxes', 'fakeURL4', 'noChildren'),
+    new Document (5, 'file5', 'file for school', 'fakeURL', 'noChildren')
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+
   }
 
 }
---
+++ cms/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,17 @@
+import {Directive, HostListener, HostBinding} from '@angular/core';
+
+@Directive({
+  selector: '[cmsDropdown]'
+})
+
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+
+  @HostListener('mouseleave') toggleClosed() {
+    this.isOpen = !this.isOpen;
+  }
+}
